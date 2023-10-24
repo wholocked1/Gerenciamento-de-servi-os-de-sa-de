0bc9b6fd33d157f0bbe086277455e2276ff1b02c
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2023-10-24 16:09:08 +0000

    começo da criação de função de cadastro (criação)

--- funcoes.c
+++ funcoes.c
@@ -1,3 +1,26 @@
-#import <stdlib.h>
-#import <stdio.h>
-#import "funcoes.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "funcoes.h"
+
+Lista *criar_lista(){ //função que cria a lista sem nenhuma informação
+    Lista *l = malloc(sizeof(Lista));
+    l->inicio = NULL;
+    l->qtde = 0;
+    return l;
+}
+
+Elista *cria_info(){
+    Elista *cliente = malloc(sizeof(Elista));
+    printf("Nome: ");
+    scanf("%s", cliente->dados.nome);
+    printf("Idade: ");
+    scanf("%d", cliente->dados.idade);
+    printf("RG: ");
+    scanf("%s", cliente->dados.rg);
+    printf("Dia: ");
+    scanf("%d", cliente->dados.data.dia);
+    printf("Mês: ");
+    scanf("%d", cliente->dados.data.mes);
+    printf("Ano: ");
+    scanf("%d", cliente->dados.data.ano);
+}
--- funcoes.h
+++ funcoes.h
@@ -1,12 +1,23 @@
-typedef struct{
+typedef struct{ //salva as informações da data de criação
     int dia;
     int mes;
     int ano;
-}Data; //salva a data
+}Data;
 
-typedef struct{
+typedef struct{ //salva as informações pessoais do cliente
     char nome[100];
     int idade;
     char rg[8]; //não conta com os pontos e traços
     Data data;
 } Registro;
+
+typedef struct Elista{ //struct que vai salvar os dados do cliente e marcar o próximo
+    Registro dados;
+    struct Elista *prox;
+}Elista;
+
+typedef struct Lista //struct que vai salvar o inicio da lista e a quantidade de pessoas na fila
+{
+    Elista *inicio;
+    int qtde;
+}Lista;
